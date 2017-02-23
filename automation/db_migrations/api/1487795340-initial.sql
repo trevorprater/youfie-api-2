63e1273aed043a4a58fe37f824bee1831d8a0137
Trevor Prater <trevor.prater@gmail.com>
2017-02-22 22:22:05 -0500

    inserts default user

--- automation/db_migrations/api/1487795340-initial.sql
+++ automation/db_migrations/api/1487795340-initial.sql
@@ -20,3 +20,4 @@ CREATE TABLE users (
     UNIQUE(email, display_name)
 );
 
+insert into users(email, hash, display_name) VALUES ('trevor@youfie.io', '$2a$10$BW7ryN6m2lM8z7f57H69a.CXdQozmgrml20tf82lDE193nAozqKEa', 'trevor');
