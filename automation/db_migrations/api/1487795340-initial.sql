cb1b230fba363050fb54ac665ec418c71168d132
Trevor Prater <trevor.prater@enigma.io>
2017-03-07 12:52:19 -0500

    Updates schema

--- automation/db_migrations/api/1487795340-initial.sql
+++ automation/db_migrations/api/1487795340-initial.sql
@@ -31,21 +31,7 @@ CREATE TABLE photos(
     storage_url TEXT,
     lat NUMERIC(10, 6),
     lng NUMERIC(10, 6),
-
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp
-);
-
-
-CREATE TABLE video(
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    format TEXT NOT NULL,
-    content BYTEA NOT NULL,
-    width INT NOT NULL,
-    height INT NOT NULL,
-    storage_url TEXT,
-    lat NUMERIC(10, 6),
-    lng NUMERIC(10, 6),
+    processed BOOLEAN DEFAULT false,
 
     created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp
@@ -54,7 +40,7 @@ CREATE TABLE video(
 CREATE TABLE faces(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     photo_id UUID NOT NULL REFERENCES photos(id),
-    feature_vector decimal[128] NOT NULL,
+    feature_vector DECIMAL[128] NOT NULL,
 
     bb_top_left_x INT NOT NULL,
     bb_top_left_y INT NOT NULL,
@@ -73,9 +59,9 @@ CREATE TABLE matches(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     photo_id UUID NOT NULL REFERENCES photos (id),
     face_id UUID NOT NULL REFERENCES faces (id),
-    user_id UUID NOT NULL,
+    user_id UUID NOT NULL REFERENCES users (id),
     confidence DECIMAL,
-    confirmed BOOLEAN,
+    confirmed BOOLEAN NOT NULL DEFAULT false,
 
     created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp
